dbf3f9ef7857378b963cb07028ae4c66f288174d
Dawid Sklodowski <davesqu@gmail.com>
2013-12-26 17:36:10 +0000

    God as a service

--- cookbooks/utilities/recipes/default.rb
+++ cookbooks/utilities/recipes/default.rb
@@ -1,8 +1,11 @@
+require 'pathname'
+
 package 'vim'
 package 'htop'
 package 'git'
 package 'git-core'
 package 'mongodb'
+#package 'ruby-dev'
 
 service 'mongodb' do
   action :restart
@@ -35,4 +38,23 @@ file "/home/deploy/.ssh/authorized_keys2" do
   mode 0644
   content File.read('/root/.ssh/authorized_keys2')
   action :create
+end
+
+gem_package 'bundler'
+gem_package 'god'
+
+file '/etc/init.d/god' do
+  owner "root"
+  group "root"
+  mode 0755
+  content (Pathname.new(File.expand_path(__FILE__)) + '..' + 'templates' + 'god').read
+end
+
+service 'god' do
+  init_command '/etc/init.d/god'
+  supports :start => true, :restart => true, :stop => true
+  action [:enable, :start]
+  start_command '/etc/init.d/god start'
+  restart_command '/etc/init.d/god restart'
+  stop_command '/etc/init.d/god stop'
 end
